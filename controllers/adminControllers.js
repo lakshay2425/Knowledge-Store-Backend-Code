ab2d8727ffd203c4e6993ab1d566462a41290296
Lakshay <lakshay12290@gmail.com>
2025-07-10 12:55:48 +0530

    Optimize adminController code

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -1,75 +1,74 @@
 const bookModel = require("../models/bookInfo");
 const orders = require("../models/order");
+const createHttpError = require("http-errors");
 
  // Function to insert book details in the database
-module.exports.bookDetails = async (req,res) => {
+module.exports.bookDetails = async (req,res, next) => {
     try{
     const {price, quantity ,genre, book_name, author, img_link, description, reviews, rating, type} = req.body;
     if(!price || !quantity || !genre || !book_name || !author || !img_link || !type){
-      return res.status(400).json({
-        message : "All Fields are necessary",
-        success : false
-      })
+      return returnError(400, "Missing required fields", next);
     }
-    const response = await bookModel.create({price, quantity ,genre, title : book_name, author, type, imageLink : img_link, type, description, rating, reviews});
-    res.json(response);
-    } catch (err){
-      res.status(500).json({
-        success : false,
-        error : err.message
-      });
+    const bookInfo = await bookModel.create({price, quantity ,genre, title : book_name, author, type, imageLink : img_link, description, rating, reviews});
+    return res.status(201).json({
+      success: true,
+      message: "Book created successfully",
+      id: bookInfo._id
+    })
+    } catch (error){
+      console.log(error.message, "Error in creating new book in db")
+      returnError(500, "Internal Server Error in creating book", next);
     }
   };
 
 
 //Function to delete a book from the database
-module.exports.deleteBook = async (req,res) => {
+module.exports.deleteBook = async (req,res, next) => {
   try {
     const {bookName} = req.params;
     if(!bookName){
-      return res.status(400).json({
-        message : "Book name is required",
-        success : false
-      })
+      return returnError(400, "Missing required fields", next);
     }
 
     const regexBook = new RegExp(bookName, 'i'); // Case-insensitive regular expression
-    const response = await bookModel.findOneAndDelete({title : regexBook});
-    res.json(response);
+    const deleteBookInfo = await bookModel.findOneAndDelete({title : regexBook});
+    return res.status(200).json({
+      success: true,
+      message: "Book deleted successfully",
+      book: deleteBookInfo
+    })
   } catch (error) {
-    res.status(500).json({
-      success : false,
-      error : error.message
-    });
+      console.log(error.message, "Error in deleting book from db")
+      returnError(500, "Internal Server Error in deleting bookInfo", next);
   }
 };
 
 //Function to update book details in the database
-module.exports.updateBook = async (req,res) => {
+module.exports.updateBook = async (req,res, next) => {
   try {
     const {author, genres, price, title, quantity, imageLink} = req.body;
     if(!price || !quantity || !genres || !title || !imageLink){
-      return res.status(400).json({
-        message : "All Fields are necessary",
-        success : false
-      })
+      return returnError(400, "Missing required fields", next);
     }
-    const response = await bookModel.findOneAndUpdate(
+    const updatedBookInfo = await bookModel.findOneAndUpdate(
       {title : title},
       {price, quantity ,genre : genres, title , author, imageLink},
       {new : true}
     );
-    res.json(response);
+    return res.status(200).json({
+      success: true,
+      message: "Book updated successfully",
+      id: updatedBookInfo._id
+    })
   } catch (error) {
-    res.status(500).json({
-      success : false,
-      error : error.message
-    });
+    console.log(error.message, "Error in updating book info in db")
+      returnError(500, "Internal Server Error in updating bookInfo", next);
   }
 }
 
-module.exports.fetchAllUserOrders = async (req,res)=>{
-  const orderDetails = await orders.find();
+module.exports.fetchAllUserOrders = async (req,res, next)=>{
+  try {
+    const orderDetails = await orders.find();
   if(orderDetails.length == 0){
     return res.status(200).json({
       message : "No orders yet",
@@ -82,4 +81,15 @@ module.exports.fetchAllUserOrders = async (req,res)=>{
       orders : orderDetails.length
     })
   }
+  } catch (error) {
+    console.error("Error in fetching all user orders", error.message);
+    returnError(500, "Internal Server Error in fetching orders", next);  
+  }
 }
+
+
+const returnError = (statusCode, message, next)=>{
+  const err = createHttpError(statusCode, message);
+    err.additionalFields = {success: false} 
+    next(err);
+};
