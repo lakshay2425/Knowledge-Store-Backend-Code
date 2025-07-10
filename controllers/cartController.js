089ac1af1257bff0c4715a34e2b46206d86a0eae
Lakshay <lakshay12290@gmail.com>
2025-07-10 10:37:05 +0530

    Optimize cart controller code

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,118 +1,121 @@
 const cartModel = require("../models/cart");
 const itemSchema = require("../utilis/joiValidator");
-const bookModel = require("../models/bookInfo");
 const {deleteAllProducts} = require("../utilis/deleteAll");
 const wishlistModel = require("../models/wishlist"); 
 const {deleteBook} = require("../utilis/deleteAll");
+const createHttpError = require("http-errors");
 
 const validateInput = (bookName, email) => {
   const { error, value } = itemSchema.validate({ bookName, email });
   if (error) {
+    console.log(`Invalid Input ${bookName, email}`,error.message)
     throw new Error(error.details[0].message);
   }
   return value;
 };
 
 
-async function addBookToCart(bookName, email, rentalPeriod) {
+async function addBookToCart(bookName, email, rentalPeriod, next) {
   try {
-    //Use Joi.attempt() to validate the data against your schema
     const value = validateInput(bookName, email);
     if(value){
       const existingBook = await cartModel.findOne({ bookName, email });
-      if (existingBook) {
-        return {
-          message: "Book already exists in the cart",
-          success: false,
-          exist: true
-        }; // Book already exists in the cart
-      }else{
-        const cart = await cartModel.create({ bookName, email, days: rentalPeriod });
+      if (!existingBook) {
+        await cartModel.create({ bookName, email, days: rentalPeriod });
         return {
           message: "Book added to cart",
           success: true,
           exist: false
         }; 
       }
+      return {
+          message: "Book already exists in the cart",
+          success: false,
+          exist: true
+        };
     }
-  } catch (error) {
-        return {
-          message: "Error while adding book to cart",
+  } catch (error){
+    console.log("Error in adding book to cart",error.message)
+    const err = createHttpError(500, "Internal Server Error")
+    err.additonalFields = {
           success: false,
           exist: false
-        }; 
+    }    
+    next(err);
   }
 }
 
 //Function to update number of days in cart
-module.exports.updateCart = async (req,res)=>{
+module.exports.updateCart = async (req,res,next)=>{
   const {days, bookName} = req.body;
   
   if(!days || !bookName){
-    return res.status(400).json({
-      message : "Days and bookName is required",
-      success : false,
-      updated: false,
-    })
+    console.log("Missing days and bookName")
+    const err = createHttpError(400, "Missing days and bookName")
+    err.additonalFields = {
+        success: false,
+        updated: false
+    }    
+    return next(err)
   }
 
-  const bookDetails = await cartModel.findOneAndUpdate({title : bookName}, {days}, {new : true});
-  if(bookDetails){
-    return res.status(200).json({
-      message : "Number of days updated in Book information successfully",
-      success : true,
-      updated: true,
-    })
-  }else{
-    return res.status(500).json({
-      message : "Failed to update Number of days in Book information",
-      success : false,
-      updated: false,
-    })
+  try{
+    const bookDetails = await cartModel.findOneAndUpdate({title : bookName}, {days}, {new : true});
+    if(bookDetails){
+      return res.status(200).json({
+        message : "Number of days updated in Book information successfully",
+        success : true,
+        updated: true,
+     })
+  }}
+  catch(err){
+    console.log("Error in updating cart", err.message);
+    const error = createHttpError(500, "Missing days and bookName")
+    error.additonalFields = {
+        success: false,
+        updated: false
+    }
+    next(error);
   }
 }
 
 //Function to add a book to user cart section
-module.exports.addToCart = async (req, res) => {
+module.exports.addToCart = async (req, res,next) => {
   try {
     const { bookName, email , rentalPeriod} = req.body; // assuming you have user data available in req.user
     if (!bookName || !email ||!rentalPeriod){
-      return res.status(400).json({ error: "Book name, rentalPeriod and email is required" });
+      console.log("Missing required fields") 
+      return next(createHttpError(400, "Missing required fields"))
     };
-    const result = await addBookToCart(bookName, email, rentalPeriod);
-    if (result.success) {
-      return res.status(201).json({
-        message: "Book added to cart successfully",
-        success: true,
-        exist: false
-      })
-    } else {
-      if(result.exist){
+    const result = await addBookToCart(bookName, email, rentalPeriod,next);
+    if (!result.success) {
+     if(result.exist){
         return res.status(409).json({ 
         message: "Book already exist in cart",
         success: false,
         exist: true
         })
-      }else{
-        return res.status(404).json({
-          success: false,
-          message: "Falied to add book to cart",
-          exist: false
-        })
-      } 
+      }
     }
+    return res.status(201).json({
+        message: "Book added to cart successfully",
+        success: true,
+        exist: false
+      })
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    console.log("Failed to add book to cart", error.message);
+    return next(createHttpError(500, "Internal Server Error"));
   }
 };
 
 //Function to fetch  book details from user cart section
-module.exports.fetchCartData = async (req, res) => {
+module.exports.fetchCartData = async (req, res, next) => {
   try {
     const email = req.body.email;
     if (!email) {
-      return res.status(400).json({ error: "Email is required" });
+      return next(createHttpError(400, "Email is required"))
     }; 
+
     const userCartWithBookInfo = await cartModel.aggregate([
         { $match: { email: email } },
         {
@@ -146,41 +149,40 @@ module.exports.fetchCartData = async (req, res) => {
         numberOfBooks: userCartWithBookInfo.length
       });
   } catch (error) {
-    res.status(500).json({
-      success: false,
-      error: `Server error ${error.message}` });
-  }
-};
+    console.log(error.message, "Failed to fetch user cart data");
+    const err = createHttpError(500, "Failed to fetch user cart data")
+    err.additonalFields = {success: false};
+    next(err);
+  };
+}
 
 
 //Function to delete a book from user cart section
-module.exports.deleteCartProduct = async (req, res) => {
+module.exports.deleteCartProduct = async (req, res,next) => {
   try {
     const { bookName, email } = req.query;
     if (!email || !bookName) {
-      return res.status(404).json({
-        message: "Email and bookName is required",
-        success: false
-      })
+      const err = createHttpError(400, "Missing required parameters")
+      err.additonalFields = {success: false};
+      return next(err);
     };
     await deleteBook(bookName, email, "Cart", res);
   } catch (error) {
-    res.status(500).json({
-      error: `Server error ${error.message}`,
-      success: false
-    });
+    console.log(error.message, "Error in deleting book from user's cart")  
+    const err = createHttpError(500, "Error in deleting book from user's cart due to internal server error")
+    err.additonalFields = {success: false};
+    next(err);
   }
 }
 
-module.exports.moveBookFromWishlisttoCart = async (req, res) => {
+module.exports.moveBookFromWishlisttoCart = async (req, res,next) => {
   try {
     const { bookName, email } = req.body;
-    
+
     if (!email || !bookName) {
-      return res.status(404).json({
-        message : "Email and bookName is required", 
-        success: false
-      })
+      const err = createHttpError(400, "Missing required parameters")
+      err.additonalFields = {success: false};
+      return next(err);
     }
     
     await wishlistModel.findOneAndDelete({
@@ -189,41 +191,23 @@ module.exports.moveBookFromWishlisttoCart = async (req, res) => {
         { email }
       ]
     });
-    const result = await addBookToCart(bookName, email);
-    if (result.success) {
-      return res.status(201).json({
-        message: "Book added to cart successfully",
-        success: true,
-        exist: false
-      })
-    } else {
+    const result = await addBookToCart(bookName, email,next);
+    if (!result.success) {
       if(result.exist){
-        await wishlistModel.create({
-          email,
-          bookName
-        })
         return res.status(409).json({ 
         message: "Book already exist in cart",
         success: false,
         exist: true
         })
-      }else{
-        await wishlistModel.create({
-          email,
-          bookName
-        });
-        return res.status(404).json({
-          success: false,
-          message: "Falied to add book to cart",
-          exist: false
-        });
-      } 
-    }
+      }
+    } 
+    return res.status(201).json({
+        message: "Book added to cart successfully",
+        success: true,
+        exist: false
+      })
   } catch (error) {
-    await wishlistModel.create({
-      email,
-      bookName
-    });
+    console.error("Error in moving book from wishlist to cart", error.message)
     return res.status(500).json({
       message: error.message,
       success: false
@@ -232,24 +216,24 @@ module.exports.moveBookFromWishlisttoCart = async (req, res) => {
 };
 
 
-module.exports.deleteAllCartProduct = async (req, res) => {
+module.exports.deleteAllCartProduct = async (req, res,next) => {
   try {
     const { email } = req.query;
     if (!email) {
-      return res.status(400).json({
-        message: "Email is required",
-        success: false
-      })
+      console.error("Email is missing");
+      const err = createHttpError(400, "Email is missing");
+      err.additonalFields = {success: false}
+      return next(err);
     }
-    const response = await deleteAllProducts(email, "cart");
+    await deleteAllProducts(email, "cart");
     return res.status(200).json({
       message : "All cart products deleted successfully",
       success : true
     })
-  } catch (error) {
-     return res.status(500).json({
-      success: false,
-      message: "Error in deleting all products, Try again later"
-    })
+  } catch (error){
+    console.log("Error ind deleting all cart product", error.message);
+    const err = createHttpError(500, "Internal Server Error");
+    err.additonalFields = {success: false};
+    next(err);
   }
 }
