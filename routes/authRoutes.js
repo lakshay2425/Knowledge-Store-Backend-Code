506dccae5a8ddabef366e94d604cea2fd2842c18
Lakshay <lakshay12290@gmail.com>
2025-07-15 11:13:02 +0530

    Remove Signup Route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const {loginDetails, logoutUser} = require('../controllers/authController');
-const {insertSignupDetails} = require("../utilis/authenticationOperations/verifySignupDetails")
+
 
 router.post('/loginDetails', loginDetails);  
 router.get('/logout', logoutUser);
-router.post('/signupDetails', insertSignupDetails);
   
 
 
