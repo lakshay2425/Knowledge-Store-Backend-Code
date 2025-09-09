bf64a4b8c9c8e873d22070e3c6e2d9dd67f08e70
Lakshay <lakshay12290@gmail.com>
2025-09-09 18:40:21 +0530

    Comment path package related code in app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from'express';
-import path from'path';
+// import path from'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import cors from 'cors';
@@ -53,7 +53,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 app.use("/api",indexRoute);
 
 
