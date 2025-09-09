b31db71a53957333d760032bf8b639ee190df813
Lakshay <lakshay12290@gmail.com>
2025-09-09 18:40:48 +0530

    Update file path in indexRoute.js

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,30 +1,31 @@
 import express from 'express';
 const router = express.Router();
 
-//Importing middlewares
-import { createRateLimiter } from "../middlewares/rateLimiter.js"
 
 //Importing routes
 import formRoutes from "./formRoutes.js";
-import  wishlistRoutes from "./wishlistRoutes.js"
-import cartRoutes from "./cartRoutes.js";
-import adminRoutes from "./adminRoutes.js";
+import  wishlistRoutes from "../wishlist/wishlistRoutes.js"
+import cartRoutes from "../cart/cartRoutes.js";
+import adminRoutes from "../admin/adminRoutes.js";
 import orderRoutes from "./ordersRoute.js";
-import userRoutes from "./users.js";
+import userRoutes from "../users/users.js";
 import paymentRoutes from "./paymentRoutes.js";
-import indexRouter from "./index.js";
+import indexRouter from "../book/index.js";
 
+//Importing middlewares
+import { createRateLimiter } from "../middlewares/rateLimiter.js"
+import { authenticateUser } from '../middlewares/authMidlleware.js';
 
 const  limiter = createRateLimiter(30, 1);
 
 
 
-router.use("/forms", limiter, formRoutes)
+router.use("/forms", limiter, authenticateUser, formRoutes)
 router.use('/', limiter, indexRouter);
 router.use("/admin", adminRoutes);
 router.use("/user", limiter, userRoutes);
-router.use("/wishlist", limiter, wishlistRoutes);
-router.use("/cart", limiter, cartRoutes);
+router.use("/wishlist", limiter, authenticateUser,wishlistRoutes);
+router.use("/cart", limiter, authenticateUser,cartRoutes);
 router.use("/orders", limiter, orderRoutes);
 router.use("/payment", paymentRoutes)
 
